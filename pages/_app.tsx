b24f9ad737d02068fcb570b0f9178ec9b16fec3c
Shawn Andrews <Shawn_Andrews@intuit.com>
2021-08-15 13:53:59 -0400

    updated history replaceState to work with nextjs

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,19 @@
+import { useEffect } from 'react'
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { useRouter } from 'next/router'
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter()
+
+  useEffect(() => {
+    const redirect = sessionStorage.redirect
+    delete sessionStorage.redirect
+    if (redirect && redirect != location.href) {
+      router.replace(redirect)
+    }
+  }, [])
+
   return <Component {...pageProps} />
 }
 export default MyApp
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,18 +5,14 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <script
+          {/* <script
             type="text/javascript"
             dangerouslySetInnerHTML={{
               __html: `(function(){
-            var redirect = sessionStorage.redirect;
-            delete sessionStorage.redirect;
-            if (redirect && redirect != location.href) {
-            history.replaceState(null, null, redirect);
-            }
-        })();`,
+                    
+                })();`,
             }}
-          ></script>
+          ></script> */}
         </Head>
         <body>
           <Main />
